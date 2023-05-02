43a1265db6c20fabb097f0582c65344c9fa18e05
Yowanda Frisky Amada <yowanmada@gmail.com>
2023-05-02 18:29:24 +0700

    comment build in gtingnore

---
+++ build/app/generated/source/buildConfig/release/com/example/starlife/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.starlife;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.starlife";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
